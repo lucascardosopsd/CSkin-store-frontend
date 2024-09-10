105ab90cae9077af08205efa3f8e28c1729b10d7
lucas cardoso <lucascardosopsd@gmail.com>
2024-09-10 14:35:44 -0300

    update: filters validator

--- src/app/validators/filters.ts
+++ src/app/validators/filters.ts
@@ -1,10 +1,10 @@
 import { z } from "zod";
 
 export const filtersValidator = z.object({
-  name: z.string(),
-  orderBy: z.string(),
-  category: z.string(),
-  startPrice: z.number().nullable(),
-  endPrice: z.number().nullable(),
-  float: z.number().nullable(),
+  name: z.string().optional(),
+  orderBy: z.string().optional(),
+  category: z.string().optional(),
+  startPrice: z.number().nullable().optional(),
+  endPrice: z.number().nullable().optional(),
+  float: z.number().nullable().optional(),
 });
